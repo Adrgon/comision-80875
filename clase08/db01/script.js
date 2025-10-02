0fcdb8833b96c2c514e3b2d362ef3a0daa3850ab
Gonzalez Adrian <adrgon@gmail.com>
2025-10-02 08:24:39 -0300

    Clase08 finalizada

---
+++ clase08/db01/script.js
@@ -0,0 +1,50 @@
+//alert()
+const JSON_FILE = "data.json"
+
+function createCharacter(character){
+    return `
+        <div class="character-card">
+            <img class="character-image" src="${character.image}" alt=${character.name}/>
+            <h3 class="character-name"> ${character.name}</h3>
+            <div>
+                ${ 
+                character.race
+                ? `<div class='character-info'><span class='info-label'>Raza: </span><span class='info-value race-tag'>${character.race}</span></div>`
+                : ''
+                }
+                ${
+                    character.gender
+                    ? `<div class="info-item"><span class="info-label">Genero: </span><span class="info-value race-tab">${character.gender}</span></div>`
+                    : ''
+                }
+            </div>
+        </div>
+    `
+}
+
+function loadCharacters() {
+    const container = document.querySelector("#characters-container")
+
+    fetch(JSON_FILE)
+    .then((respuesta)=>{
+        console.log(respuesta)
+        if(!respuesta.ok){
+            throw new Error("Error " + respuesta.status)
+        }
+        return respuesta.json()
+    })
+    .then((datos)=>{
+        console.log(datos)
+        const characters = datos.items
+        console.log(characters)
+        container.innerHTML = characters.map((character) => createCharacter(character))
+
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+
+
+loadCharacters()
---
+++ clase08/db03/script.js
@@ -0,0 +1,55 @@
+//alert()
+const API_URL = "https://dragonball-api.com/api/character";
+
+function createCharacter(character){
+    return `
+        <div class="character-card">
+            <img class="character-image" src="${character.image}" alt=${character.name}/>
+            <h3 class="character-name"> ${character.name}</h3>
+            <div>
+                ${ 
+                character.race
+                ? `<div class='character-info'><span class='info-label'>Raza: </span><span class='info-value race-tag'>${character.race}</span></div>`
+                : ''
+                }
+                ${
+                    character.gender
+                    ? `<div class="info-item"><span class="info-label">Genero: </span><span class="info-value race-tab">${character.gender}</span></div>`
+                    : ''
+                }
+            </div>
+        </div>
+    `
+}
+
+async function loadCharacters() {
+    const container = document.querySelector("#characters-container");
+    const error = document.querySelector("#error-message");
+    
+    error.style.display = "none";
+    container.style.display = "block";
+
+
+    try {
+      const response = await fetch(API_URL); // tarda
+    if (!response.ok) {
+      throw new Error("Error " + response.status);
+    }
+      const data = await response.json();
+      console.log(data);
+      const characters = data.items;
+      container.innerHTML = characters
+        .map((character) => createCharacter(character))
+        .join("");
+    } catch (err) {
+        error.style.display = "block"
+        container.style.display = "none"
+        document.querySelector('#error-message').textContent = err.message
+    }
+
+
+}
+
+
+
+loadCharacters()
